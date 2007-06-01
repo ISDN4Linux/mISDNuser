b3e7ed67e037e3ce3c5b30a881577905135bd712
root <root@beroTester.(none)>
2007-06-01 15:13:57 +0200

    have a single script for all branches

--- checkout-portability.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-#checks out the portability branch
-
-git-checkout -b portability origin/portability
-git-config branch.portability.remote origin
-git-config branch.portability.merge refs/heads/portability
-
